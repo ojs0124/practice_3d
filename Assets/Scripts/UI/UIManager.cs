8ba5ddcb05c21238b4839b534fdaf5c9f8b8ae5b
ojs0124 <ojs_0124_@naver.com>
2024-10-31 12:59:24 +0900

    c

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+
+    public static UIManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameObject("UIManager").AddComponent<UIManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    private GameObject UI = Resources.Load<GameObject>("UI");
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
